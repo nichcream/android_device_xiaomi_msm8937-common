6fd494956bc26527b5039ec8b9bb56ac9e6ff410
Rajaram Gudivada <rajaramg@codeaurora.org>
2016-09-07 11:31:10 +0530

    QCamera2: HAL1: Force Flash Off only when StillMore alone is enabled
    Issue:
    Flash is turned Off in SeeMore even though UI enabled it.
    It's because, if SeeMore is enabled then StillMore is forcibly
    enabled from HAL to get the same effect for LiveSnapshot.
    But due to this Flash is turned Off assuming StillMore is
    On.
    
    Fix:
    Set flash Off only when StillMore is enabled and SeeMore is Off.
    By this Flash is controlled only when StillMore alone is enabled.
    
    Change-Id: I4baf10377d5b91bd5a3042b2bb081ee447ad9b1a
    CRs-Fixed: 1062032
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- camera/QCamera2/HAL/QCameraParameters.cpp
+++ camera/QCamera2/HAL/QCameraParameters.cpp
@@ -9460,8 +9460,9 @@ int32_t QCameraParameters::updateFlash(bool commitSettings)
       }
     }
 
+    // Turn Off Flash if any of the below AOST features are enabled
     if (isHDREnabled() || m_bAeBracketingEnabled || m_bAFBracketingOn ||
-          m_bOptiZoomOn || m_bReFocusOn ) {
+          m_bOptiZoomOn || m_bReFocusOn || (m_bStillMoreOn && !m_bSeeMoreOn)) {
         value = CAM_FLASH_MODE_OFF;
     } else if (m_bChromaFlashOn) {
         value = CAM_FLASH_MODE_ON;
