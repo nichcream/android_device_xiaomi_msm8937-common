25c4948e838aa2c3c24b4f0f324991d10387bbc2
Satish kumar sugasi <ssugas@codeaurora.org>
2016-09-27 17:54:37 -0700

    land: bluetooth: Increase MAX ACL connections to 16
    This allows 16 ACL links instead of 7
    
    Change-Id: I7bf04515b5822c51be73ec95cba7975e046bca23

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -35,7 +35,7 @@ inline const char* BtmGetDefaultName()
 
 #define BTM_DEF_LOCAL_NAME BtmGetDefaultName()
 
-#define MAX_ACL_CONNECTIONS    7
+#define MAX_ACL_CONNECTIONS    16
 #define MAX_L2CAP_CHANNELS    16
 #define BLE_VND_INCLUDED   TRUE
 // skips conn update at conn completion
