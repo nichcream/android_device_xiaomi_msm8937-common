0f2bcce38b3f87988ae562503add72d52b45ce8f
Matadeen Mishra <matade@codeaurora.org>
2016-10-14 10:57:20 +0530

    land: BT: cleanup of unused FLAG
    Cleanup of unused FLAG.
    
    Change-Id: I39a24cf7253876a2e146338c8676c9b4eac66d98
    CRs-fixed: 1077888

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -35,18 +35,9 @@ inline const char* BtmGetDefaultName()
 
 #define BTM_DEF_LOCAL_NAME BtmGetDefaultName()
 
-#define BTA_SKIP_BLE_READ_REMOTE_FEAT FALSE
 #define MAX_ACL_CONNECTIONS    7
 #define MAX_L2CAP_CHANNELS    16
 #define BLE_VND_INCLUDED   TRUE
-#define BTM_SCO_ENHANCED_SYNC_DISABLED TRUE
-#define BTM_WBS_INCLUDED TRUE
-#define BTIF_HF_WBS_PREFERRED TRUE
 // skips conn update at conn completion
-#define BTA_BLE_SKIP_CONN_UPD  FALSE
-#define BLE_PERIPHERAL_ADV_NAME  FALSE
 #define BT_CLEAN_TURN_ON_DISABLED 1
-#define BTA_DISABLE_DELAY 1000 /* in milliseconds */
-#define BTM_WBS_INCLUDED TRUE
-#define BTIF_HF_WBS_PREFERRED TRUE
 #endif
