8198c981fb743ca0135c0292a77efc8b64346758
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-10-29 22:13:55 +0800

    land: Add initial CyanogenMod makefile

---
+++ cm.mk
@@ -0,0 +1,39 @@
+#
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common CM stuff
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit from land device
+$(call inherit-product, device/xiaomi/land/device.mk)
+
+PRODUCT_BRAND := Xiaomi
+PRODUCT_DEVICE := land
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_MODEL := Redmi 3S
+PRODUCT_NAME := cm_land
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+TARGET_VENDOR_PRODUCT_NAME := land
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=Xiaomi/land/land:6.0.1/MMB29M/V8.0.3.0.MALMIDG:user/release-keys \
+    PRIVATE_BUILD_DESC="land-user 6.0.1 MMB29M V8.0.3.0.MALMIDG release-keys"
