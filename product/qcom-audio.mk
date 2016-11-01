c45fe29d6b82b14f52f639820018e804cc39c825
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-11-01 22:02:33 +0800

    land: audio: Add missing audio configs
    Change-Id: I78d38cd72524b7aead4c44f56c36ac77e3fda365

--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -1,3 +1,8 @@
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
+    $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml
+
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.qc.sdk.audio.fluencetype=fluence \
