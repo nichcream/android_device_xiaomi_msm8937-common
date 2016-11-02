e22834324565b513c88f39645a11327b7b2681b3
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-11-02 20:33:42 +0800

    land: Use the correct mixer paths
    Change-Id: I2217bfb4945be64d177991a8e2a669dba597f1df

--- product/audio.mk
+++ product/audio.mk
@@ -4,7 +4,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_output_policy.conf:system/vendor/etc/audio_output_policy.conf \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+    $(LOCAL_PATH)/audio/mixer_paths_qrd_sku1.xml:system/etc/mixer_paths_qrd_sku1.xml
 
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
