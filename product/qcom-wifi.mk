55b174632d48c2ba9a705cfb0e090c829c9566e6
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-11-05 23:18:16 +0800

    land: Add missing wifi fstman ini file
    Change-Id: Iabf9b1e9cfab21911164695c04103b957310ed2e

--- product/qcom-wifi.mk
+++ product/qcom-wifi.mk
@@ -28,4 +28,5 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
-    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini
+    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
+    $(LOCAL_PATH)/wifi/fstman.ini:system/etc/wifi/fstman.ini
