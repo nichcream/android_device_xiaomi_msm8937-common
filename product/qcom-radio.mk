e24a056a0eae7639939c0fcdba480d83c9a5e6b0
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-11-06 18:22:52 +0800

    land: Set missing radio props
    Change-Id: I9659df9b394c6b1550be1e9d90743e7a494a30a0

--- product/qcom-radio.mk
+++ product/qcom-radio.mk
@@ -10,16 +10,30 @@ PRODUCT_COPY_FILES += \
 
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
+    DEVICE_PROVISIONED=1 \
+    persist.data.iwlan.enable=false \
     persist.data.mode=concurrent \
     persist.data.netmgrd.qos.enable=true \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.vt_avail_ovr=1 \
+    persist.radio.DROPSETENABLE=1 \
     persist.radio.apm_sim_not_pwdn=1 \
     persist.radio.custom_ecc=1 \
+    persist.radio.force_on_dc=true \
+    persist.radio.ignore_dom_time=5 \
+    persist.radio.mt_sms_ack=20 \
     persist.radio.multisim.config=dsds \
+    persist.radio.rat_on=combine \
     persist.radio.sib16_support=1 \
+    ril.subscription.types=NV,RUIM \
     rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
+    ro.build.display.wtid=SW_S88537AA1_V053_M20_MP_XM \
+    ro.ct.device.model=XMP-2016030 \
+    ro.product.wt.boardid=S88537AA1 \
     ro.telephony.call_ring.multiple=false \
     ro.telephony.default_network=20 \
-    ro.use_data_netmgrd=true
+    ro.use_data_netmgrd=true \
+    telephony.lteOnCdmaDevice=1
 
 # RIL
 PRODUCT_PACKAGES += \
