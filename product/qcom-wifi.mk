a9e51dd41aadc04fd4114ea25f219399fb28c2c1
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-11-09 07:10:38 +0800

    land: Move Wi-Fi configs to vendor
    Change-Id: Iebbf0b662bd2a1ff03832f93a4d6e255e6eb0f8e

--- product/qcom-wifi.mk
+++ product/qcom-wifi.mk
@@ -7,26 +7,12 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     wifi.interface=wlan0
 
-# Wifi
+# Wi-Fi
 PRODUCT_PACKAGES += \
-    libQWiFiSoftApCfg \
     libqsap_sdk \
     libwpa_client
 
 PRODUCT_PACKAGES += \
-    dhcpcd.conf \
     hostapd \
     wpa_supplicant \
     wpa_supplicant.conf
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/wifi/hostapd.accept:system/etc/hostapd/hostapd.accept \
-    $(LOCAL_PATH)/wifi/hostapd_default.conf:system/etc/hostapd/hostapd_default.conf \
-    $(LOCAL_PATH)/wifi/hostapd.deny:system/etc/hostapd/hostapd.deny \
-    $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
-    $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
-    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
-    $(LOCAL_PATH)/wifi/fstman.ini:system/etc/wifi/fstman.ini
