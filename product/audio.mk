07ef0bf215a904573859533356d84146ec4170fb
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-11-10 11:29:30 +0800

    land: Move audio configs to vendor
    Change-Id: I5b061be2be2f752c333dd836f9f72964c759a8aa

--- product/audio.mk
+++ product/audio.mk
@@ -1,21 +1,23 @@
-# Audio configs
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
-    $(LOCAL_PATH)/audio/audio_output_policy.conf:system/vendor/etc/audio_output_policy.conf \
-    $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
-    $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/audio/mixer_paths_qrd_sku1.xml:system/etc/mixer_paths_qrd_sku1.xml
-
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
     af.fast_track_multiplier=2 \
     audio.deep_buffer.media=true \
+    audio.dolby.ds2.enabled=true \
     audio.offload.buffer.size.kb=64 \
     audio.offload.gapless.enabled=true \
     audio.offload.min.duration.secs=30 \
     audio.offload.pcm.16bit.enable=true \
     audio.offload.pcm.24bit.enable=true \
     audio.offload.video=true \
+    audio.playback.mch.downsample=true \
+    audio.safx.pbe.enabled=true \
     audio_hal.period_size=192 \
+    ro.qc.sdk.audio.fluencetype=fluence \
     persist.audio.dirac.speaker=true \
-    use.voice.path.for.pcm.voip=true
+    persist.audio.fluence.voicecall=true \
+    use.qti.sw.alac.decoder=true \
+    use.qti.sw.ape.decoder=true \
+    use.voice.path.for.pcm.voip=true \
+    voice.conc.fallbackpath=deep-buffer \
+    voice.playback.conc.disabled=true \
+    voice.voip.conc.disabled=true
