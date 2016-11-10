ba0f865fac89fd8df3a44c14531b56e49a3f563e
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-11-10 20:01:17 +0800

    land: Remove unnecessary kernel board flags
    Change-Id: I72e85a2ab6e93921955bebd6a273120867ab1b73

--- board/kernel.mk
+++ board/kernel.mk
@@ -5,8 +5,6 @@ BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 an
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
-TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CONFIG := cyanogenmod_land_defconfig
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8937
 TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/kernel
