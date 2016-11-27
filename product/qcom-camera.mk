680722cfc74d558b3c66f0fddeac5c9fcb5a6850
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-11-28 07:38:47 +0800

    land: Use boot time video timestamp reference
    This matches what the camera drivers use.
    
    Change-Id: I4951d31d09fc7952e5dfc49347200972aa9eeaee

--- product/qcom-camera.mk
+++ product/qcom-camera.mk
@@ -1,5 +1,6 @@
 # ZRAM
 PRODUCT_PROPERTY_OVERRIDES += \
     camera.hal1.packagelist=com.skype.raider,com.google.android.talk \
+    media.camera.ts.monotonic=0 \
     persist.camera.gyro.android=1 \
     persist.camera.is_type=1
