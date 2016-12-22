bdfe681d60c984936a63126e6c3b3089f4941a13
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-12-22 18:44:52 +0800

    Revert "land: Enable console"
    No longer enabled in miui_HM3SGlobal_6.12.15_9ae6840ff8_6.0.
    
    This reverts commit 4a140764375aa84e2fa9d2c58511def42f471be1.

--- board/kernel.mk
+++ board/kernel.mk
@@ -1,7 +1,7 @@
 # kernel
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_CUSTOM_BOOTIMG_MK := $(DEVICE_PATH)/mkbootimg.mk
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78B0000
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78B0000
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
