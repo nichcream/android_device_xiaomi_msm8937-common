9ef01e64c77e8019f213ab07f1233ba0a6f87811
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-12-22 11:21:49 +0800

    land: Send doze pulse intent on tilt detected
    Based on oneplus3's implementation.
    
    Change-Id: Ibc4bd629f8280d63f84d23981e01cc861992c17f

--- product/display.mk
+++ product/display.mk
@@ -11,6 +11,10 @@ PRODUCT_AAPT_PREF_CONFIG := xhdpi
 # A list of dpis to select prebuilt apk, in precedence order.
 PRODUCT_AAPT_PREBUILT_DPI := hdpi
 
+# Ambient display
+PRODUCT_PACKAGES += \
+    CMDoze
+
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapgrowthlimit=192m \
