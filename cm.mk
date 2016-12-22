efbcb4d2523a314bf9d8c1bee12b6debf3daebd7
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-12-22 18:51:46 +0800

    land: Update fingerprint
    MIUI version: miui_HM3SGlobal_6.12.15_9ae6840ff8_6.0
    
    Change-Id: I8a49fc9b07e2b051470bd22d7f24aa448280ee09

--- cm.mk
+++ cm.mk
@@ -38,5 +38,5 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR_PRODUCT_NAME := land
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT=Xiaomi/land/land:6.0.1/MMB29M/6.11.3:user/release-keys \
-    PRIVATE_BUILD_DESC="land-user 6.0.1 MMB29M 6.11.3 release-keys"
+    BUILD_FINGERPRINT=Xiaomi/land/land:6.0.1/MMB29M/6.12.15:user/release-keys \
+    PRIVATE_BUILD_DESC="land-user 6.0.1 MMB29M 6.12.15 release-keys"
