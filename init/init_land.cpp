74ebcefa33229313fa9a45f4e48aec7f85a46f16
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-12-25 10:15:56 +0800

    land: Update system props
    MIUI verison: miui_HM3SGlobal_6.12.22_550d48eac0_6.0
    
    Change-Id: I411bf331cfa16197ea544347a73145aae282ed96

--- init/init_land.cpp
+++ init/init_land.cpp
@@ -121,17 +121,17 @@ void vendor_load_properties()
     property_set("ro.product.wt.boardid", board_id);
 
     if (!strcmp(board_id, "S88537AA1")) {
-        property_set("ro.build.display.wtid", "SW_S88537AA1_V069_M20_MP_XM");
+        property_set("ro.build.display.wtid", "SW_S88537AA1_V070_M20_MP_XM");
     } else if (!strcmp(board_id, "S88537AB1")) {
-        property_set("ro.build.display.wtid", "SW_S88537AB1_V069_M20_MP_XM");
+        property_set("ro.build.display.wtid", "SW_S88537AB1_V070_M20_MP_XM");
     } else if (!strcmp(board_id, "S88537AC1")) {
-        property_set("ro.build.display.wtid", "SW_S88537AC1_V069_M20_MP_XM");
+        property_set("ro.build.display.wtid", "SW_S88537AC1_V070_M20_MP_XM");
     } else if (!strcmp(board_id, "S88537BA1")) {
-        property_set("ro.build.display.wtid", "SW_S88537BA1_V069_M20_MP_XM");
+        property_set("ro.build.display.wtid", "SW_S88537BA1_V070_M20_MP_XM");
     } else if (!strcmp(board_id, "S88537CA1")) {
-        property_set("ro.build.display.wtid", "SW_S88537CA1_V069_M20_MP_XM");
+        property_set("ro.build.display.wtid", "SW_S88537CA1_V070_M20_MP_XM");
     } else if (!strcmp(board_id, "S88537EC1")) {
-        property_set("ro.build.display.wtid", "SW_S88537EC1_V069_M20_MP_XM");
+        property_set("ro.build.display.wtid", "SW_S88537EC1_V070_M20_MP_XM");
     }
 
     init_alarm_boot_properties();
