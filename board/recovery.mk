3e464c2ae70b6df2001891afa332c971099b241e
Zhao Wei Liew <zhaoweiliew@gmail.com>
2017-01-01 13:18:43 +0800

    land: Move common recovery makefile to device
    This is in no way common.
    
    Change-Id: Ib249fd067bb4f5dd7f5800783f8dc234b2b099ea

--- board/recovery.mk
+++ board/recovery.mk
@@ -1,4 +1,5 @@
 # Recovery
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/fstab.qcom
 TARGET_RECOVERY_UI_LIB := librecovery_ui_msm
 TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_msm
 TARGET_RELEASETOOLS_EXTENSIONS := device/qcom/common
