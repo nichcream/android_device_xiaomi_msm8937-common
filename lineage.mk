cb7aa8d5055c1aecbd1ee1279d02198e473df0bd
Zhao Wei Liew <zhaoweiliew@gmail.com>
2017-01-08 18:51:12 +0800

    land: Rebrand to LineageOS
    Change-Id: I1db1dc8c93032f49a048e43654e0779bab4485e6

--- lineage.mk
+++ lineage.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2016 The CyanogenMod Project
+#               2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -21,7 +22,7 @@ $(call inherit-product, $(LOCAL_PATH)/PlatformConfig.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common CM stuff
+# Inherit some common Lineage stuff
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit from land device
@@ -30,7 +31,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := land
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := cm_land
+PRODUCT_NAME := lineage_land
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
