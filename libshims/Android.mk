685e387e0478f8fb4c48c48d7f0c4b462aa84283
LuK1337 <priv.luk@gmail.com>
2017-01-26 00:48:19 +0100

    land: Add libc hax for camera
     * See : https://review.lineageos.org/#/c/1170/
    
    Change-Id: I040c3023f81322140d153cb14be9578457777ac2
    Signed-off-by: klozz <klozz707@gmail.com>
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -49,3 +49,14 @@ LOCAL_SHARED_LIBRARIES := libbinder
 LOCAL_MODULE := libshim_parcel
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := \
+    bionic/bionic_time_conversions.cpp \
+    bionic/pthread_cond.cpp
+
+LOCAL_SHARED_LIBRARIES := libc
+LOCAL_MODULE := libshims_camera
+LOCAL_MODULE_TAGS := optional
+LOCAL_32_BIT_ONLY := true
+include $(BUILD_SHARED_LIBRARY)
