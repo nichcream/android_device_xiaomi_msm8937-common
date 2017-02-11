870d26fd15fbc644613553ec3515a90294e7ffb5
Zhao Wei Liew <zhaoweiliew@gmail.com>
2017-02-12 05:24:36 +0800

    land: Import OSS camera HAL
    CAF revision: AU_LINUX_ANDROID_LA.UM.5.1_RB1.06.00.01.192.038
    
    Change-Id: I87589fea5e9c00c3bdc9c79cc6b2d6453c3d09b3

--- camera/QCamera2/stack/mm-camera-interface/Android.mk
+++ camera/QCamera2/stack/mm-camera-interface/Android.mk
@@ -36,7 +36,7 @@ LOCAL_C_INCLUDES := \
     $(LOCAL_PATH)/inc \
     $(LOCAL_PATH)/../common \
     hardware/libhardware/include/hardware \
-    $(call project-path-for,qcom-media)/mm-core/inc \
+    hardware/qcom/media/mm-core/inc \
     system/media/camera/include \
 
 LOCAL_CFLAGS += -DCAMERA_ION_HEAP_ID=ION_IOMMU_HEAP_ID
