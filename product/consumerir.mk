e479695e4e48b2c481fa9cc298a4bd2a133a328a
Hriday Sharma <hridaysharma42@gmail.com>
2017-04-19 16:48:48 +0000

    land: Build OSS consumerir HAL

--- product/consumerir.mk
+++ product/consumerir.mk
@@ -1,3 +1,7 @@
+# Consumerir
+PRODUCT_PACKAGES += \
+    consumerir.msm8937
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:system/etc/permissions/android.hardware.consumerir.xml
