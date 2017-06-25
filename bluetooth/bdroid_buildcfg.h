991250df6f6a2b620e58fddfa2cc557ce5659726
Luca Stefani <luca020400@lineageos.org>
2017-06-25 12:56:07 +0200

    land: Append 'Xiaomi' to BT name

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -27,10 +27,10 @@ inline const char* BtmGetDefaultName()
 	property_get("ro.product.wt.boardid", board_id, "");
 
 	if (!strcmp("S88537AB1", board_id)) {
-		return "Redmi 3X";
+		return "Xiaomi Redmi 3X";
 	}
 
-	return "Redmi 3S";
+	return "Xiaomi Redmi 3S";
 }
 
 #define BTM_DEF_LOCAL_NAME BtmGetDefaultName()
