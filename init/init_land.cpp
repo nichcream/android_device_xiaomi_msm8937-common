65323f0062835ed74a3fb0564807dd0c53bcd5ab
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2017-07-27 20:16:52 -0500

    land: Update init from stock and enable mm.enable.qcom_parser as predefined on stock miui 8.5
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- init/init_land.cpp
+++ init/init_land.cpp
@@ -121,17 +121,20 @@ void vendor_load_properties()
     property_set("ro.product.wt.boardid", board_id);
 
     if (!strcmp(board_id, "S88537AA1")) {
-        property_set("ro.build.display.wtid", "SW_S88537AA1_V077_M20_MP_XM");
+        property_set("ro.build.display.wtid", "SW_S88537AA1_V079_M20_MP_XM");
     } else if (!strcmp(board_id, "S88537AB1")) {
-        property_set("ro.build.display.wtid", "SW_S88537AB1_V077_M20_MP_XM");
+        property_set("ro.build.display.wtid", "SW_S88537AB1_V079_M20_MP_XM");
     } else if (!strcmp(board_id, "S88537AC1")) {
-        property_set("ro.build.display.wtid", "SW_S88537AC1_V077_M20_MP_XM");
+        property_set("ro.build.display.wtid", "SW_S88537AC1_V079_M20_MP_XM");
     } else if (!strcmp(board_id, "S88537BA1")) {
-        property_set("ro.build.display.wtid", "SW_S88537BA1_V077_M20_MP_XM");
+        property_set("ro.build.display.wtid", "SW_S88537BA1_V079_M20_MP_XM");
+        property_set("mm.enable.qcom_parser","196495");
     } else if (!strcmp(board_id, "S88537CA1")) {
-        property_set("ro.build.display.wtid", "SW_S88537CA1_V077_M20_MP_XM");
+        property_set("ro.build.display.wtid", "SW_S88537CA1_V079_M20_MP_XM");
+        property_set("mm.enable.qcom_parser","196495");
     } else if (!strcmp(board_id, "S88537EC1")) {
-        property_set("ro.build.display.wtid", "SW_S88537EC1_V077_M20_MP_XM");
+        property_set("ro.build.display.wtid", "SW_S88537EC1_V079_M20_MP_XM");
+        property_set("mm.enable.qcom_parser","196495");
     }
 
     if (!strcmp(board_id, "S88537AB1")) {
