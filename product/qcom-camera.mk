81a9a0aef8589ae129cc1634595352bbc9515050
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2017-08-01 01:16:34 -0500

    land:update some props
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- product/qcom-camera.mk
+++ product/qcom-camera.mk
@@ -5,6 +5,12 @@ PRODUCT_PACKAGES += \
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
     camera.hal1.packagelist=com.skype.raider,com.google.android.talk,com.whatsapp,com.google.android.GoogleCamera \
-    media.camera.ts.monotonic=1 \
+    media.camera.ts.monotonic=0 \
     persist.camera.gyro.android=1 \
-    persist.camera.is_type=1
+    persist.camera.is_type=1 \
+    vidc.debug.perf.mode=2 \
+    vidc.enc.dcvs.extra-buff-count=2 \
+    vidc.enc.disable.pq=true \
+    media.stagefright.legacyencoder=true \
+    media.stagefright.less-secure=true \
+    persist.camera.HAL3.enabled=1 
