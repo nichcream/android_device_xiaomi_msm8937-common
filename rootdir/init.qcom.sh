7519f9839a401f7cf887f44a534ea6daf07968cd
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2017-08-10 12:21:50 -0500

    land: Update ramdisk change permission of some files
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- rootdir/init.qcom.sh
+++ rootdir/init.qcom.sh
@@ -34,7 +34,7 @@ else
 fi
 
 #
-# Function to start sensors for DSPS enabled platforms
+# Function to start sensors for SSC enabled platforms
 #
 start_sensors()
 {
@@ -48,6 +48,53 @@ start_sensors()
 
         start sensors
     fi
+}
+
+start_sensors
+
+start_battery_monitor()
+{
+	if ls /sys/bus/spmi/devices/qpnp-bms-*/fcc_data ; then
+		chown -h root.system /sys/module/pm8921_bms/parameters/*
+		chown -h root.system /sys/module/qpnp_bms/parameters/*
+		chown -h root.system /sys/bus/spmi/devices/qpnp-bms-*/fcc_data
+		chown -h root.system /sys/bus/spmi/devices/qpnp-bms-*/fcc_temp
+		chown -h root.system /sys/bus/spmi/devices/qpnp-bms-*/fcc_chgcyl
+		chmod 0660 /sys/module/qpnp_bms/parameters/*
+		chmod 0660 /sys/module/pm8921_bms/parameters/*
+		mkdir -p /data/bms
+		chown -h root.system /data/bms
+		chmod 0770 /data/bms
+		start battery_monitor
+	fi
+}
+
+start_charger_monitor()
+{
+	if ls /sys/module/qpnp_charger/parameters/charger_monitor; then
+		chown -h root.system /sys/module/qpnp_charger/parameters/*
+		chown -h root.system /sys/class/power_supply/battery/input_current_max
+		chown -h root.system /sys/class/power_supply/battery/input_current_trim
+		chown -h root.system /sys/class/power_supply/battery/input_current_settled
+		chown -h root.system /sys/class/power_supply/battery/voltage_min
+		chmod 0664 /sys/class/power_supply/battery/input_current_max
+		chmod 0664 /sys/class/power_supply/battery/input_current_trim
+		chmod 0664 /sys/class/power_supply/battery/input_current_settled
+		chmod 0664 /sys/class/power_supply/battery/voltage_min
+		chmod 0664 /sys/module/qpnp_charger/parameters/charger_monitor
+		start charger_monitor
+	fi
+}
+
+start_vm_bms()
+{
+	if [ -e /dev/vm_bms ]; then
+		chown -h root.system /sys/class/power_supply/bms/current_now
+		chown -h root.system /sys/class/power_supply/bms/voltage_ocv
+		chmod 0664 /sys/class/power_supply/bms/current_now
+		chmod 0664 /sys/class/power_supply/bms/voltage_ocv
+		start vm_bms
+	fi
 }
 
 start_msm_irqbalance_8939()
@@ -78,20 +125,216 @@ start_copying_prebuilt_qcril_db()
 baseband=`getprop ro.baseband`
 echo 1 > /proc/sys/net/ipv6/conf/default/accept_ra_defrtr
 
-start_sensors
-start_msm_irqbalance_8939
+case "$baseband" in
+        "svlte2a")
+        start bridgemgrd
+        ;;
+esac
 
-if [ -f /sys/class/graphics/fb0/modes ]; then
-	panel_res=`cat /sys/class/graphics/fb0/modes`
-	if [ "${panel_res:5:1}" == "x" ]; then
-		panel_xres=${panel_res:2:3}
-	else
-		panel_xres=${panel_res:2:4}
-	fi
-fi
+case "$target" in
+    "msm7630_surf" | "msm7630_1x" | "msm7630_fusion")
+        if [ -f /sys/devices/soc0/hw_platform ]; then
+            value=`cat /sys/devices/soc0/hw_platform`
+        else
+            value=`cat /sys/devices/system/soc/soc0/hw_platform`
+        fi
+        case "$value" in
+            "Fluid")
+             start profiler_daemon;;
+        esac
+        ;;
+    "msm8660" )
+        if [ -f /sys/devices/soc0/hw_platform ]; then
+            platformvalue=`cat /sys/devices/soc0/hw_platform`
+        else
+            platformvalue=`cat /sys/devices/system/soc/soc0/hw_platform`
+        fi
+        case "$platformvalue" in
+            "Fluid")
+                start profiler_daemon;;
+        esac
+        ;;
+    "msm8960")
+        case "$baseband" in
+            "msm")
+                start_battery_monitor;;
+        esac
 
-# Set shared touchpanel nodes ownership (these are proc_symlinks to the real sysfs nodes)
-chown -LR system.system /proc/touchpanel
+        if [ -f /sys/devices/soc0/hw_platform ]; then
+            platformvalue=`cat /sys/devices/soc0/hw_platform`
+        else
+            platformvalue=`cat /sys/devices/system/soc/soc0/hw_platform`
+        fi
+        case "$platformvalue" in
+             "Fluid")
+                 start profiler_daemon;;
+             "Liquid")
+                 start profiler_daemon;;
+        esac
+        ;;
+    "msm8974")
+        platformvalue=`cat /sys/devices/soc0/hw_platform`
+        case "$platformvalue" in
+             "Fluid")
+                 start profiler_daemon;;
+             "Liquid")
+                 start profiler_daemon;;
+        esac
+        case "$baseband" in
+            "msm")
+                start_battery_monitor
+                ;;
+        esac
+        start_charger_monitor
+        ;;
+    "apq8084")
+        platformvalue=`cat /sys/devices/soc0/hw_platform`
+        case "$platformvalue" in
+             "Fluid")
+                 start profiler_daemon;;
+             "Liquid")
+                 start profiler_daemon;;
+        esac
+        ;;
+    "msm8226")
+        start_charger_monitor
+        ;;
+    "msm8610")
+        start_charger_monitor
+        ;;
+    "msm8916")
+        start_vm_bms
+        start_msm_irqbalance_8939
+        if [ -f /sys/devices/soc0/soc_id ]; then
+            soc_id=`cat /sys/devices/soc0/soc_id`
+        else
+            soc_id=`cat /sys/devices/system/soc/soc0/id`
+        fi
+
+        if [ -f /sys/devices/soc0/platform_subtype_id ]; then
+             platform_subtype_id=`cat /sys/devices/soc0/platform_subtype_id`
+        fi
+        if [ -f /sys/devices/soc0/hw_platform ]; then
+             hw_platform=`cat /sys/devices/soc0/hw_platform`
+        fi
+        case "$soc_id" in
+             "239")
+                  case "$hw_platform" in
+                       "Surf")
+                            case "$platform_subtype_id" in
+                                 "1")
+                                      setprop qemu.hw.mainkeys 0
+                                      ;;
+                            esac
+                            ;;
+                       "MTP")
+                          case "$platform_subtype_id" in
+                               "3")
+                                    setprop qemu.hw.mainkeys 0
+                                    ;;
+                          esac
+                          ;;
+                  esac
+                  ;;
+        esac
+        ;;
+    "msm8994" | "msm8992")
+        start_msm_irqbalance
+        ;;
+    "msm8996")
+        if [ -f /sys/devices/soc0/hw_platform ]; then
+             hw_platform=`cat /sys/devices/soc0/hw_platform`
+        fi
+        case "$hw_platform" in
+                "MTP" | "CDP")
+                #Loop through the sysfs nodes and determine the correct sysfs to change the permission and ownership.
+                        for count in 0 1 2 3 4 5 6 7 8 9 10
+                        do
+                                dir="/sys/devices/soc/75ba000.i2c/i2c-12/12-0020/input/input"$count
+                                if [ -d "$dir" ]; then
+                                     chmod 0660 $dir/secure_touch_enable
+                                     chmod 0440 $dir/secure_touch
+                                     chown system.drmrpc $dir/secure_touch_enable
+                                     chown system.drmrpc $dir/secure_touch
+                                     break
+                                fi
+                        done
+                        ;;
+        esac
+        ;;
+    "msm8909")
+        start_vm_bms
+        ;;
+    "msm8937")
+        start_msm_irqbalance_8939
+        if [ -f /sys/devices/soc0/soc_id ]; then
+            soc_id=`cat /sys/devices/soc0/soc_id`
+        else
+            soc_id=`cat /sys/devices/system/soc/soc0/id`
+        fi
+
+        if [ -f /sys/devices/soc0/hw_platform ]; then
+             hw_platform=`cat /sys/devices/soc0/hw_platform`
+        else
+             hw_platform=`cat /sys/devices/system/soc/soc0/hw_platform`
+        fi
+        case "$soc_id" in
+             "294" | "295" | "303" | "307" | "308" | "309")
+                  case "$hw_platform" in
+                       "Surf")
+     #                               setprop qemu.hw.mainkeys 0
+                                    ;;
+                       "MTP")
+     #                               setprop qemu.hw.mainkeys 0
+                                    ;;
+                       "RCM")
+     #                               setprop qemu.hw.mainkeys 0
+                                    ;;
+                  esac
+                  ;;
+       esac
+        ;;
+    "msm8953")
+	start_msm_irqbalance_8939
+        if [ -f /sys/devices/soc0/soc_id ]; then
+            soc_id=`cat /sys/devices/soc0/soc_id`
+        else
+            soc_id=`cat /sys/devices/system/soc/soc0/id`
+        fi
+
+        if [ -f /sys/devices/soc0/hw_platform ]; then
+             hw_platform=`cat /sys/devices/soc0/hw_platform`
+        else
+             hw_platform=`cat /sys/devices/system/soc/soc0/hw_platform`
+        fi
+        case "$soc_id" in
+             "293" | "304" )
+                  case "$hw_platform" in
+                       "Surf")
+                                    setprop qemu.hw.mainkeys 0
+                                    ;;
+                       "MTP")
+                                    setprop qemu.hw.mainkeys 0
+                                    ;;
+                       "RCM")
+                                    setprop qemu.hw.mainkeys 0
+                                    ;;
+                  esac
+                  ;;
+       esac
+        ;;
+esac
+
+bootmode=`getprop ro.bootmode`
+emmc_boot=`getprop ro.boot.emmc`
+case "$emmc_boot"
+    in "true")
+        if [ "$bootmode" != "charger" ]; then # start rmt_storage and rfs_access
+            start rmt_storage
+            start rfs_access
+        fi
+    ;;
+esac
 
 #
 # Copy qcril.db if needed for RIL
@@ -126,43 +369,4 @@ if [ ! -d /persist/alarm ]; then
     chown system:system /persist/alarm
     restorecon /persist/alarm
 fi
-
-# Set Memory paremeters.
-#
-# Set per_process_reclaim tuning parameters
-# 2GB 64-bit will have aggressive settings when compared to 1GB 32-bit
-# 1GB and less will use vmpressure range 50-70, 2GB will use 10-70
-# 1GB and less will use 512 pages swap size, 2GB will use 1024
-#
-# Set Low memory killer minfree parameters
-# 32 bit all memory configurations will use 15K series
-# 64 bit all memory configurations will use 18K series
-#
-# Set ALMK parameters (usually above the highest minfree values)
-# 32 bit will have 53K & 64 bit will have 81K
-#
-
-# Read adj series and set adj threshold for PPR and ALMK.
-# This is required since adj values change from framework to framework.
-#adj_series=`cat /sys/module/lowmemorykiller/parameters/adj`
-#adj_1="${adj_series#*,}"
-#set_almk_ppr_adj="${adj_1%%,*}"
-
-# PPR and ALMK should not act on HOME adj and below.
-# Normalized ADJ for HOME is 6. Hence multiply by 6
-# ADJ score represented as INT in LMK params, actual score can be in decimal
-# Hence add 6 considering a worst case of 0.9 conversion to INT (0.9*6).
-#set_almk_ppr_adj=$(((set_almk_ppr_adj * 6) + 6))
-#echo $set_almk_ppr_adj > /sys/module/lowmemorykiller/parameters/adj_max_shift
-#echo $set_almk_ppr_adj > /sys/module/process_reclaim/parameters/min_score_adj
-
-#echo 1 > /sys/module/process_reclaim/parameters/enable_process_reclaim
-#echo 70 > /sys/module/process_reclaim/parameters/pressure_max
-#echo 30 > /sys/module/process_reclaim/parameters/swap_opt_eff
-#echo 1 > /sys/module/lowmemorykiller/parameters/enable_adaptive_lmk
-
-#echo 10 > /sys/module/process_reclaim/parameters/pressure_min
-#echo 1024 > /sys/module/process_reclaim/parameters/per_swap_size
-#echo "18432,23040,27648,32256,55296,80640" > /sys/module/lowmemorykiller/parameters/minfree
-#echo 81250 > /sys/module/lowmemorykiller/parameters/vmpressure_file_min
 
