ccbd5f8daf7fbccd0058d2424e78dafca4ffdcdd
Vitaliy Tomin <highwaystar.ru@gmail.com>
2017-08-30 21:04:31 +0800

    land: add seccomp policy
    Change-Id: I2dcc7b256044dcf8db997359b3c4f785daae4cab

--- device.mk
+++ device.mk
@@ -281,6 +281,10 @@ PRODUCT_PACKAGES += \
     librmnetctl \
     libxml2
 
+# Seccomp policy
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
