ec3af0b25ea8b0e9b53168eccba2910f76c507bc
Bruno Martins <bgcngm@gmail.com>
2017-09-09 00:52:49 +0100

    land: init: Use core init function to read from files
    Change-Id: I3fd23490bcdfa4097dad73161ad226337e93cd18
    Signed-off-by: klozz <klozz707@gmail.com>

--- init/Android.mk
+++ init/Android.mk
@@ -18,7 +18,9 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_C_INCLUDES := system/core/init
+LOCAL_C_INCLUDES := \
+    system/core/base/include \
+    system/core/init
 LOCAL_MODULE := libinit_land
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := init_land.cpp
