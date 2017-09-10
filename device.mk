742d143d0520b1d231f48e5f31238b9695eb8b40
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2017-09-10 01:00:10 -0500

    land: add treble hidl path support for sensors, add Graphics HIDL HAL
    add sensor manifest file to run sensors as seperate service.
    
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- device.mk
+++ device.mk
@@ -143,8 +143,10 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
+    android.hardware.renderscript@1.0-impl \
     copybit.msm8937 \
     gralloc.msm8937 \
     hwcomposer.msm8937 \
