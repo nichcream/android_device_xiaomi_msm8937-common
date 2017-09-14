04c49cadcf334dfc093746993a2ac1658dd8ef53
Michael Bestas <mikeioannina@cyanogenmod.org>
2017-09-14 15:55:35 -0300

    land: Allow vendor init to override props
    Change-Id: I8a88292e8dbe4c6460c38bf4cde93097e7ed2516

--- init/init_land.cpp
+++ init/init_land.cpp
@@ -32,6 +32,8 @@
 #include <android-base/logging.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
 #include <iostream>
 #include <string>
 #include <sstream>
@@ -45,6 +47,17 @@
 #include "property_service.h"
 #include "log.h"
 #include "util.h"
+
+void property_override(char const prop[], char const value[])
+{
+    prop_info *pi;
+
+    pi = (prop_info*) __system_property_find(prop);
+    if (pi)
+        __system_property_update(pi, value, strlen(value));
+    else
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+}
 
 static std::string board_id;
 using android::base::GetProperty;
