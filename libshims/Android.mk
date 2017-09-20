96bd8e358dd12b1ca8e502df85f5876678cdc250
klozz <klozz707@gmail.com>
2017-09-19 22:50:01 -0400

    land: update ims libshim
    Change-Id: Ia671bec8e58090aad7f9543cc441baa2955cf5ce
    Signed-off-by: klozz <klozz707@gmail.com>

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -15,8 +15,10 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := MediaBuffer.c
-LOCAL_SHARED_LIBRARIES := libstagefright_foundation
+LOCAL_SRC_FILES := MediaBuffer.cpp
+LOCAL_SHARED_LIBRARIES          += libui libgui libstagefright_foundation
+LOCAL_C_INCLUDES                += framework/native/include frameworks/av/include
+LOCAL_CFLAGS                    += -Wno-unused-private-field
 LOCAL_MODULE := libshims_ims
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
