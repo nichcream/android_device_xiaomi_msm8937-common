6ff022358dabf50d1d9c63f90ab4bea568e26026
klozz <klozz707@gmail.com>
2017-09-30 06:05:52 -0400

    land: fix libshim_camera
    add libsensor
    
    Signed-off-by: klozz <klozz707@gmail.com>

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -27,7 +27,7 @@ LOCAL_SRC_FILES := \
     gui/SensorManager.cpp
 
 LOCAL_C_INCLUDES := gui
-LOCAL_SHARED_LIBRARIES := libgui libutils liblog libbinder libandroid
+LOCAL_SHARED_LIBRARIES := libgui libutils liblog libbinder libandroid libsensor
 LOCAL_MODULE := libshim_camera
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 include $(BUILD_SHARED_LIBRARY)
