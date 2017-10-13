481ae7e5213872666cc4367a936b2c308842fe16
Manaf Meethalavalappu Pallikunhi <manafm@codeaurora.org>
2017-10-13 15:16:44 +0530

    land: Add a dummy Thermal HAL for VTS
    Add default Thermal HAL module with bypass methods for target which
    doesn't need Thermal HAL APIs implementation. It makes sure VTS for
    Thermal HAL will be passed for these targets.
    
    Change-Id: If1d3fceaae1002c24367d2ff264bef34025f4a4c

--- device.mk
+++ device.mk
@@ -390,8 +390,9 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-   android.hardware.thermal@1.0-impl \
-   android.hardware.thermal@1.0-service
+    thermal.msm8937 \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service
 
 # USB HAL
 PRODUCT_PACKAGES += \
