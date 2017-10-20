1a5aeb8cd793ae154dee385943ef163a21ed7edd
nichcream <c.potato23@gmail.com>
2017-10-21 00:23:53 +0800

    land: Additional fixes to build under GZR env
    * Call caf_fw makefile to fix RIL
    -- As per suggestion from https://plus.google.com/+FaradisDrubo/posts/j4yBdqbNUox
    
    * Fix bluetooth_loader SEPolicy build
    
    * sepolicy: Permissive
    -- Way too many denials. Fixes mounting external storage and MANY
    others.

--- validus.mk
+++ validus.mk
@@ -19,8 +19,9 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff
+# Inherit some common Validus stuff
 $(call inherit-product, vendor/validus/config/common_full_phone.mk)
+$(call inherit-product, vendor/validus/config/caf_fw.mk)
 
 # Inherit from land device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
