bdf0b3f15aeb8155ad4e1515879b0206ffed9122
wj <c.potato23@gmail.com>
2017-11-08 16:21:57 +0800

    land: Rollback biometrics to 2.0
    2.1 not working yet.

--- device.mk
+++ device.mk
@@ -161,7 +161,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service
+    android.hardware.biometrics.fingerprint@2.0-service
 
 # FM
 PRODUCT_PACKAGES += \
