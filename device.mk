fb015d424c672dbd4f444a2b8fb8011d0dd57ca4
Bindu Gokul <bgokul@codeaurora.org>
2017-11-28 16:03:58 +0530

    land: remove android.hardware.usb; no usbpd support
    Change-Id: Icb46be06afd4f23252439e481e36741cde346612

--- device.mk
+++ device.mk
@@ -379,10 +379,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
-# USB
-PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
-
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
