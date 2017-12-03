178ee1d22e79aa62cabdb6aae81e1631992ffc98
nichcream <c.potato23@gmail.com>
2017-12-03 15:32:09 +0800

    land: Fix building

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -219,8 +219,8 @@ QCOM_DISPLAY_VARIANT := msm8937
 
 # Recovery
 TARGET_RECOVERY_FSTAB 		 := $(DEVICE_PATH)/rootdir/fstab.qcom
-TARGET_RECOVERY_UI_LIB		 := librecovery_ui_msm
-TARGET_RECOVERY_UPDATER_LIBS  	 := librecovery_updater_msm
+#TARGET_RECOVERY_UI_LIB		 := librecovery_ui_msm
+#TARGET_RECOVERY_UPDATER_LIBS  	 := librecovery_updater_msm
 TARGET_RELEASETOOLS_EXTENSIONS 	 := device/qcom/common
 
 # Sensors
