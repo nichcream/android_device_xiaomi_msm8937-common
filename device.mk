56709ecce93a354b3fe96b7e2f06e55b02ea6784
nichcream <c.potato23@gmail.com>
2017-12-05 21:24:02 +0800

    land: Copy all sensor configs into vendor

--- device.mk
+++ device.mk
@@ -348,7 +348,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
-    $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:system/etc/sensors/sensor_def_qcomdev.conf
+    $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
 
 # Telephony packages
 PRODUCT_PACKAGES += \
