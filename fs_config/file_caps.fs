16ee80d2be9b35ce0a262264b94840966b1eb14a
nichcream <c.potato23@gmail.com>
2017-12-11 23:26:31 +0800

    land: Replace instances of qcom_diag with oem_2901

--- fs_config/file_caps.fs
+++ fs_config/file_caps.fs
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+[AID_QTI_DIAG]
+value:2901
 
 [system/vendor/bin/imsdatadaemon]
 user: AID_SYSTEM
