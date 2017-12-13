c03eed28444497f6f6a9710bbf045adcde5368ff
nichcream <c.potato23@gmail.com>
2017-12-13 23:20:44 +0800

    land: Changes for old sensor libs

--- device.mk
+++ device.mk
@@ -345,7 +345,8 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl
+    android.hardware.sensors@1.0-impl \
+    sensors.land
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
