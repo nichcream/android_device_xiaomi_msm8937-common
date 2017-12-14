d249c749d3c121322a3c06f1ed0bb5096c042936
nichcream <c.potato23@gmail.com>
2017-12-14 23:33:21 +0800

    land: Drop GPS shims

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -68,32 +68,10 @@ LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := get_process_name.c
-
-LOCAL_MODULE := libshims_get_process_name
-LOCAL_MODULE_TAGS := optional
-
-include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := rild_socket.c
 
 LOCAL_MODULE := rild_socket
 LOCAL_MODULE_TAGS := optional
 
-include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := is_wifi_driver_loaded.cpp
-LOCAL_MODULE := libshims_is_wifi_driver_loaded
-LOCAL_MODULE_TAGS := optional
-LOCAL_SHARED_LIBRARIES := libwifi-hal
-include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := sensor.cpp
-LOCAL_MODULE := libshims_sensor
-LOCAL_SHARED_LIBRARIES := libsensor
-LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
