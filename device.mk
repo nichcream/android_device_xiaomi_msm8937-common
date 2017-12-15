2f5cede4ef1bf6dd3e35c074f8d325061ef21174
Paras Nagda <pnagda@codeaurora.org>
2017-12-15 13:28:14 +0530

    land: load google's low end media codec xml
    load google's low end media codec xml for 8937
    
    Change-Id: Ifb924fa7823b422babfa67a6f7fe935a20de0f16

--- device.mk
+++ device.mk
@@ -309,7 +309,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
+    frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
 # NET
 PRODUCT_PACKAGES += \
