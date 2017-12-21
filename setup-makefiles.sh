d91810dff06ab78343994965e3d2be67ee0e8b1f
nichcream <c.potato23@gmail.com>
2017-12-21 14:05:31 +0800

    land: Add a seperate list for common QC blobs

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -46,6 +46,8 @@ write_headers
 
 # The standard blobs
 write_makefiles "$MY_DIR"/proprietary-files.txt
+echo "" >> "$PRODUCTMK"
+write_makefiles "$MY_DIR"/proprietary-files-qc.txt
 
 cat << EOF >> "$ANDROIDMK"
 
