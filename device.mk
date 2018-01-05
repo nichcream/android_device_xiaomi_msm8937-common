70fd6e247d2c80d16919908d325e5902d8e2af2d
jhenrique09 <jhenrique09.mcz@hotmail.com>
2018-01-05 11:47:02 -0200

    land: power: Switch to 1.1 implementation

--- device.mk
+++ device.mk
@@ -336,7 +336,7 @@ PRODUCT_COPY_FILES += \
 
 # Power HAL
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-service-qti
+    android.hardware.power@1.1-service-qti
 
 # RIL
 PRODUCT_PACKAGES += \
