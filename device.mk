833bc1b030d51613013e066aeaa6fb38fa9e1aab
nichcream <c.potato23@gmail.com>
2018-01-06 02:06:33 +0800

    land: Import msm_irqbalance config

--- device.mk
+++ device.mk
@@ -278,6 +278,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl
 
+# IRQ
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/msm_irqbalance.conf:system/vendor/etc/msm_irqbalance.conf
+
 # Media
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
