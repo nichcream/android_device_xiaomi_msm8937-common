7e67b41278a91348bfebb5c9a078dbe70211901d
nichcream <c.potato23@gmail.com>
2018-01-07 09:42:55 +0800

    Revert "Revert "QCamera2: Disable QCameraDisplay temporarily""
    Facing display service crash with the new display HAL.
    
    This reverts commit 09b96c59b8523cb37963970c221e2ac00143bf0e.

--- camera/QCamera2/HAL/QCamera2HWI.h
+++ camera/QCamera2/HAL/QCamera2HWI.h
@@ -764,7 +764,7 @@ private:
     uint32_t mSurfaceStridePadding;
 
     //QCamera Display Object
-    QCameraDisplay mCameraDisplay;
+    //QCameraDisplay mCameraDisplay;
 
     bool m_bNeedRestart;
     Mutex mMapLock;
