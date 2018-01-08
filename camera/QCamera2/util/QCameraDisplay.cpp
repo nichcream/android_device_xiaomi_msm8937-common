49563bed987ba4aa24a41749ee5bc7f05c7de19b
Abhinav Sohane <asohan@codeaurora.org>
2018-01-08 11:25:57 +0530

    QCamera2:QCameraDisplay:Dynamic registration of display service.
    Issue: If display service is not available, opencamera call is got
           blocked in IDisplayService::getService call.
    Solution: As getService is blocking call, Added changes to get the
              notificaiton of display service registered and calling
              getService on same thread to avoid open camera blocking
              if display service is not registered.
    CRs-fixed: 2168244
    Change-Id: I6b8fe1729fedff49cea587b8ca20d7f101598cb2
    (cherry-picked from commit 9766baa11d87e06e40689a31e61c6319b99b109c
    from platform/hardware/qcom/camera for msm-4.9)

--- camera/QCamera2/util/QCameraDisplay.cpp
+++ camera/QCamera2/util/QCameraDisplay.cpp
@@ -126,6 +126,39 @@ QCameraDisplay*
 QCameraDisplay::mCameraDisplay = NULL;
 
 /*===========================================================================
+ * FUNCTION   : onRegistration
+ *
+ * DESCRIPTION: DisplayService registered notification callback from hwservice.
+ *              Calling init to get display service.
+ *
+ * PARAMETERS :
+ *   @fqName  : fully-qualified instance name (UNUSED).
+ *   @name    : string by which display service is registerd.
+ *   @preexisting: If true, this means the registration was
+ *                 pre-existing at the time this IServiceNotification
+ *                 object is itself registered. Otherwise, this means
+ *                 onRegistration is triggered by a newly registered
+ *                 service.
+ *
+ * RETURN     : void.Just to fullfill the type requirement of thread function.
+ *==========================================================================*/
+Return<void>
+QCameraDisplay::ServiceRegisterNotification::onRegistration(const hidl_string & /*fqName*/,
+                                                                  const hidl_string & /*name*/,
+                                                                  bool /*preexisting*/)
+{
+    if(mCameraDisplay->mDisplayService != nullptr)
+    {
+        return Return<void>();
+    }
+
+    LOGD("Display service is available, get the interface");
+    mCameraDisplay->init();
+
+    return Return<void>();
+}
+
+/*===========================================================================
  * FUNCTION   : instance
  *
  * DESCRIPTION: create singleton instance of QCameraDisplay
@@ -140,10 +173,8 @@ QCameraDisplay::instance()
     if(mCameraDisplay == NULL)
     {
         mCameraDisplay = new QCameraDisplay();
-    }
-    if(!mCameraDisplay->m_bInitDone)
-    {
-        mCameraDisplay->init();
+        mCameraDisplay->mRegistrationCB = new ServiceRegisterNotification();
+        IDisplayService::registerForNotifications("", mCameraDisplay->mRegistrationCB);
     }
     return mCameraDisplay;
 }
@@ -248,8 +279,6 @@ QCameraDisplay::QCameraDisplay()
       } else {
           mVsyncThreadCameraHandle = 0;
       }
-#else
-    init();
 #endif //USE_DISPLAY_SERVICE
 }
 
@@ -273,6 +302,7 @@ QCameraDisplay::~QCameraDisplay()
     mDeathRecipient.clear();
     mDisplayEventReceiver.clear();
     mDisplayService.clear();
+    mRegistrationCB.clear();
 #else
     mThreadExit = 1;
     if (mVsyncThreadCameraHandle != 0) {
