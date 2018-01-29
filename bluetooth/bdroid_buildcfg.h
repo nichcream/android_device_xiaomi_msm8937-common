6ddf31bf5f3ffd5ae97c3c94fc0a7a30ac557670
Fei Zheng <zfei@codeaurora.org>
2018-01-29 12:43:36 +0800

    land: Bluetooth: Enable HFP WBS feature
    Set BTIF_HF_CLIENT_WBS_INCLUDED to TRUE to support "Codec Negotiation" in HFP
    supported features.
    
    Change-Id: I481d71906908d9f0eded143be313e0e558636ed7

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -43,4 +43,7 @@ inline const char* BtmGetDefaultName()
 
 /* Increasing SEPs to 12 from 6 to support SHO/MCast i.e. two streams per codec */
 #define AVDT_NUM_SEPS 12
+
+/* Enable HFP WBS feature */
+#define BTIF_HF_CLIENT_WBS_INCLUDED TRUE
 #endif
