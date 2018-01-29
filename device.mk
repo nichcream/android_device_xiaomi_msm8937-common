f4a178a2e46dd537c1674b135f06cb0b0bf19a46
nichcream <c.potato23@gmail.com>
2018-01-29 17:41:09 +0800

    land: Remove thermal config from device tree

--- device.mk
+++ device.mk
@@ -375,10 +375,6 @@ PRODUCT_BOOT_JARS += \
     telephony-ext
 
 # Thermal
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine.conf:system/etc/thermal-engine.conf
-
-# Thermal
 PRODUCT_PACKAGES += \
    android.hardware.thermal@1.0-impl \
    android.hardware.thermal@1.0-service
