95da377dd6b67f4513f0328d54cb23b5f90b871c
nichcream <c.potato23@gmail.com>
2018-02-02 10:41:34 +0800

    doze: Rebrand to LineageOS

--- doze/src/org/lineageos/settings/doze/DozeService.java
+++ doze/src/org/lineageos/settings/doze/DozeService.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.cyanogenmod.settings.doze;
+package org.lineageos.settings.doze;
 
 import android.app.Service;
 import android.content.BroadcastReceiver;
