d5c5e1c549f96132fb767d5f40aa7a6100dcc1d2
nichcream <c.potato23@gmail.com>
2018-03-08 10:26:51 +0800

    land: Nuke aanc_tuning_mixer
     * Nuke deprecated aanc_tuning_mixer.txt which was converted for RAS test
       device under audio_tuning_mixer.txt.
    
    Change-Id: I694ec1d0f80cbb89cc707d87f032c90c6a26995c

--- device.mk
+++ device.mk
@@ -100,7 +100,6 @@ PRODUCT_PACKAGES += \
 
 # Audio mixer
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/aanc_tuning_mixer.txt:$(TARGET_COPY_OUT_VENDOR)/etc/aanc_tuning_mixer.txt \
     $(LOCAL_PATH)/audio/audio_tuning_mixer.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer.txt \
     $(LOCAL_PATH)/audio/audio_output_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_output_policy.conf \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
