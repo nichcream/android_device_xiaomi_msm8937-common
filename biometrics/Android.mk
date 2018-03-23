9da9895e5cf58ce27728a8222104993c781cd85a
nichcream <c.potato23@gmail.com>
2018-03-24 07:35:55 +0800

    land: manifest: Don't include the biometrics fingerprint HAL
    We only start this service after the fingerprint vendor has been
    determined.
    
    Change-Id: Ibb0103915be288cbdf8d0e7f939ba5c680593860

--- biometrics/Android.mk
+++ biometrics/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := android.hardware.biometrics.fingerprint@2.0-service-custom
-LOCAL_INIT_RC := android.hardware.biometrics.fingerprint@2.0-service.rc
+#LOCAL_INIT_RC := android.hardware.biometrics.fingerprint@2.0-service.rc
 LOCAL_PROPRIETARY_MODULE := true
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_SRC_FILES := \
