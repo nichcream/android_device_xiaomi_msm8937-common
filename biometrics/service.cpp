0e99bbc3580b18b8fb421899004816b3814d3811
nichcream <c.potato23@gmail.com>
2018-03-31 19:39:57 +0800

    land: Partially revert "land: biometrics: Refactor, improve and cleanup"
    We need to init fingerprintd on /dev/binder for Goodix.
    
    Change-Id: Ic5186ec58a302e5449558b8fbb673a0e226a74ec

--- biometrics/service.cpp
+++ biometrics/service.cpp
@@ -16,6 +16,7 @@
 
 #define LOG_TAG "android.biometrics.fingerprint@2.0-service.custom"
 
+#include <binder/IPCThreadState.h>
 #include <binder/IServiceManager.h>
 #include <binder/PermissionCache.h>
 #include <utils/String16.h>
@@ -63,7 +64,7 @@ int main() {
 
     if (!strcmp(vend, "goodix")) {
         /* process Binder transaction as a double-threaded program. */
-        configureRpcThreadpool(2, true /* callerWillJoin */);
+        configureRpcThreadpool(1, false /* callerWillJoin */);
     } else {
         /* process Binder transaction as a single-threaded program. */
         configureRpcThreadpool(1, true /* callerWillJoin */);
@@ -78,7 +79,11 @@ int main() {
         ALOGE("Can't create instance of BiometricsFingerprint, nullptr");
     }
 
-    joinRpcThreadpool();
+    if (!strcmp(vend, "goodix")) {
+        android::IPCThreadState::self()->joinThreadPool();   // run binder service fingerprintd part
+    } else {
+        joinRpcThreadpool();
+    }
 
     return 0; // should never get here
 }
