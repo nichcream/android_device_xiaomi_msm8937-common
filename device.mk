ee3c611f50e4c40997629764dc5563ff48c306ff
nichcream <c.potato23@gmail.com>
2018-04-24 19:31:15 +0800

    land: Initial big blobs update
    This change updates most of the QCOM common blobs from both Redmi 5 Plus
    and BQ Aquarius X Pro of which seem to be based off CAF tag
    LA.UM.6.6.r1-06700-89xx.0.
    
    Change-Id: I1404681f4845109a6718f83baad68d528716c4ce

--- device.mk
+++ device.mk
@@ -394,6 +394,9 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
+
 # USB HAL
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
