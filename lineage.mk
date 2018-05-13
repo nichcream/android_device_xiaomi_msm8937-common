7b519d3e6dd1e7b5eba6f498163ebc646f344a95
Nich <nctrenco@gmail.com>
2018-05-13 17:11:03 +0800

    land: Update blobs from V9.5.5.0.MALMIFA
    Change-Id: I2133500417a538ba7d7a5399376c315e4a28b601

--- lineage.mk
+++ lineage.mk
@@ -36,6 +36,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR_PRODUCT_NAME := land
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="land-user 6.0.1 MMB29M V9.5.1.0.MALMIFA release-keys"
+    PRIVATE_BUILD_DESC="land-user 6.0.1 MMB29M V9.5.5.0.MALMIFA release-keys"
 
-BUILD_FINGERPRINT := Xiaomi/land/land:6.0.1/MMB29M/V9.5.1.0.MALMIFA:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/land/land:6.0.1/MMB29M/V9.5.5.0.MALMIFA:user/release-keys
