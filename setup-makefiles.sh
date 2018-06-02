a9d4b560a22e72b3759e1ec8a92147aa9c5ae7ef
Nich <nctrenco@gmail.com>
2018-06-02 11:58:56 +0800

    land: Fix incomplete rebranding
    Change-Id: I19cf8f22278a5dfd2540811e519995daf73b1986

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -25,13 +25,13 @@ export INITIAL_COPYRIGHT_YEAR=2016
 DEVICE=land
 VENDOR=xiaomi
 
-# Load extractutils and do some sanity checks
+# Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-CM_ROOT="$MY_DIR"/../../..
+LINEAGE_ROOT="$MY_DIR"/../../..
 
-HELPER="$CM_ROOT"/vendor/validus/build/tools/extract_utils.sh
+HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -39,7 +39,7 @@ fi
 . "$HELPER"
 
 # Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
+setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT"
 
 # Copyright headers and guards
 write_headers
