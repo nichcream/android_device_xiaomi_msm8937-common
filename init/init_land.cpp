1879f21a4da62c495ba1975722c8dfc6b393edac
Nich <nctrenco@gmail.com>
2018-06-07 14:15:53 +0800

    land: init: Clean up and format
    -> Change unique functions/variables to static
    -> More appropriate naming of functions
    
    Change-Id: I643553772f01c93fa2472525881eb7d0c0adf881

--- init/init_land.cpp
+++ init/init_land.cpp
@@ -29,28 +29,31 @@
    IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <android-base/properties.h>
+#include <android-base/file.h>
 #include <android-base/logging.h>
+#include <android-base/properties.h>
+#include <android-base/strings.h>
 #include <fcntl.h>
+#include <iostream>
+#include <sstream>
 #include <stdlib.h>
+#include <string>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
-#include <iostream>
-#include <string>
-#include <sstream>
 #include <sys/sysinfo.h>
 
-//for cmdline parsing
-#include <android-base/file.h>
-#include <android-base/strings.h>
-
-#include "vendor_init.h"
-#include "property_service.h"
 #include "log.h"
+#include "property_service.h"
 #include "util.h"
+#include "vendor_init.h"
 
-void property_override(char const prop[], char const value[])
-{
+using android::base::GetProperty;
+using android::base::Trim;
+using android::init::property_set;
+
+static std::string board_id;
+
+static void property_override(char const prop[], char const value[]) {
     prop_info *pi;
 
     pi = (prop_info*) __system_property_find(prop);
@@ -60,40 +63,31 @@ void property_override(char const prop[], char const value[])
         __system_property_add(prop, strlen(prop), value, strlen(value));
 }
 
-static std::string board_id;
-using android::base::GetProperty;
-using android::base::Trim;
-using android::init::property_set;
-using android::init::import_kernel_cmdline;
-
-//Take care about 3gb ram
-int is3GB()
-{
+static bool is3GBram() {
     struct sysinfo sys;
     sysinfo(&sys);
     return sys.totalram > 2048ull * 1024 * 1024;
 }
 
-//cmdline parser
-void import_kernel_cmdline1(bool in_qemu,
+static void import_kernel_cmdline_land(bool in_qemu,
                            const std::function<void(const std::string&, const std::string&, bool)>& fn) {
     std::string cmdline;
     android::base::ReadFileToString("/proc/cmdline", &cmdline);
-
     for (const auto& entry : android::base::Split(android::base::Trim(cmdline), " ")) {
         std::vector<std::string> pieces = android::base::Split(entry, "=");
-        if (pieces.size() >= 2) { // lineage's : == 2
+        /* The board_id entry has two equal signs, so accept more than two pieces */
+        if (pieces.size() >= 2) { // original -> == 2
             fn(pieces[0], pieces[1], in_qemu);
         }
     }
 }
 
+static void parse_cmdline_boardid(const std::string& key,
+        const std::string& value, bool for_emulator __attribute__((unused))) {
+    if (key.empty())
+        return;
 
-static void import_cmdline(const std::string& key,
-        const std::string& value, bool for_emulator __attribute__((unused)))
-{
-    if (key.empty()) return;
-
+    /* Here our value is board_id:board_vol; we only want the first part */
     if (key == "board_id") {
         std::istringstream iss(value);
         std::string token;
@@ -102,8 +96,47 @@ static void import_cmdline(const std::string& key,
     }
 }
 
-static void init_alarm_boot_properties()
-{
+static void set_ramconfig() {
+    if (is3GBram()) {
+        property_set("dalvik.vm.heapstartsize", "8m");
+        property_set("dalvik.vm.heapgrowthlimit", "288m");
+        property_set("dalvik.vm.heapsize", "768m");
+        property_set("dalvik.vm.heaptargetutilization", "0.75");
+        property_set("dalvik.vm.heapminfree", "512k");
+        property_set("dalvik.vm.heapmaxfree", "8m");
+        property_set("ro.hwui.texture_cache_size", "72");
+        property_set("ro.hwui.layer_cache_size", "48");
+        property_set("ro.hwui.r_buffer_cache_size", "8");
+        property_set("ro.hwui.path_cache_size", "32");
+        property_set("ro.hwui.gradient_cache_size", "1");
+        property_set("ro.hwui.drop_shadow_cache_size", "6");
+        property_set("ro.hwui.texture_cache_flushrate", "0.4");
+        property_set("ro.hwui.text_small_cache_width", "1024");
+        property_set("ro.hwui.text_small_cache_height", "1024");
+        property_set("ro.hwui.text_large_cache_width", "2048");
+        property_set("ro.hwui.text_large_cache_height", "1024");
+    } else {
+        property_set("dalvik.vm.heapstartsize", "8m");
+        property_set("dalvik.vm.heapgrowthlimit", "192m");
+        property_set("dalvik.vm.heapsize", "512m");
+        property_set("dalvik.vm.heaptargetutilization", "0.75");
+        property_set("dalvik.vm.heapminfree", "2m");
+        property_set("dalvik.vm.heapmaxfree", "8m");
+        property_set("ro.hwui.texture_cache_size", "72");
+        property_set("ro.hwui.layer_cache_size", "48");
+        property_set("ro.hwui.r_buffer_cache_size", "8");
+        property_set("ro.hwui.path_cache_size", "32");
+        property_set("ro.hwui.gradient_cache_size", "1");
+        property_set("ro.hwui.drop_shadow_cache_size", "6");
+        property_set("ro.hwui.texture_cache_flushrate", "0.4");
+        property_set("ro.hwui.text_small_cache_width", "1024");
+        property_set("ro.hwui.text_small_cache_height", "1024");
+        property_set("ro.hwui.text_large_cache_width", "2048");
+        property_set("ro.hwui.text_large_cache_height", "2048");
+    }
+}
+
+static void init_alarm_boot_properties() {
     char const *boot_reason_file = "/proc/sys/kernel/boot_reason";
     char const *power_off_alarm_file = "/persist/alarm/powerOffAlarmSet";
     std::string boot_reason;
@@ -136,62 +169,18 @@ static void init_alarm_boot_properties()
     }
 }
 
-void read_ramconfig()
-{
-    if (is3GB()) {
-        property_set("dalvik.vm.heapstartsize", "8m");
-        property_set("dalvik.vm.heapgrowthlimit", "288m");
-        property_set("dalvik.vm.heapsize", "768m");
-        property_set("dalvik.vm.heaptargetutilization", "0.75");
-        property_set("dalvik.vm.heapminfree", "512k");
-        property_set("dalvik.vm.heapmaxfree", "8m");
-        //HWUI
-        property_set("ro.hwui.texture_cache_size", "72");
-        property_set("ro.hwui.layer_cache_size", "48");
-        property_set("ro.hwui.r_buffer_cache_size", "8");
-        property_set("ro.hwui.path_cache_size", "32");
-        property_set("ro.hwui.gradient_cache_size", "1");
-        property_set("ro.hwui.drop_shadow_cache_size", "6");
-        property_set("ro.hwui.texture_cache_flushrate", "0.4");
-        property_set("ro.hwui.text_small_cache_width", "1024");
-        property_set("ro.hwui.text_small_cache_height", "1024");
-        property_set("ro.hwui.text_large_cache_width", "2048");
-        property_set("ro.hwui.text_large_cache_height", "1024");
-    } else {
-        property_set("dalvik.vm.heapstartsize", "8m");
-        property_set("dalvik.vm.heapgrowthlimit", "192m");
-        property_set("dalvik.vm.heapsize", "512m");
-        property_set("dalvik.vm.heaptargetutilization", "0.75");
-        property_set("dalvik.vm.heapminfree", "2m");
-        property_set("dalvik.vm.heapmaxfree", "8m");
-        //HWUI
-        property_set("ro.hwui.texture_cache_size", "72");
-        property_set("ro.hwui.layer_cache_size", "48");
-        property_set("ro.hwui.r_buffer_cache_size", "8");
-        property_set("ro.hwui.path_cache_size", "32");
-        property_set("ro.hwui.gradient_cache_size", "1");
-        property_set("ro.hwui.drop_shadow_cache_size", "6");
-        property_set("ro.hwui.texture_cache_flushrate", "0.4");
-        property_set("ro.hwui.text_small_cache_width", "1024");
-        property_set("ro.hwui.text_small_cache_height", "1024");
-        property_set("ro.hwui.text_large_cache_width", "2048");
-        property_set("ro.hwui.text_large_cache_height", "2048");
-    }
-}
-
-void variant_properties()
-{
+static void variant_properties() {
     std::string product = GetProperty("ro.product.name", "");
     if (product.find("land") == std::string::npos)
         return;
 
-    import_kernel_cmdline1(0, import_cmdline);
+    // Get board_id from cmdline
+    import_kernel_cmdline_land(false, parse_cmdline_boardid);
 
-    //set board
+    // Set board id
     property_set("ro.product.wt.boardid", board_id.c_str());
 
-
-    //Variants
+    // Set variant based on board_id
     if (board_id == "S88537AB1") {
         property_override("ro.product.model", "Redmi 3X");
         property_override("ro.vendor.product.device", "Redmi 3X");
@@ -199,14 +188,10 @@ void variant_properties()
         property_override("ro.product.model", "Redmi 3S");
         property_override("ro.vendor.product.device", "Redmi 3S");
     }
-
 }
 
-void vendor_load_properties()
-{
-
-    // init
-    read_ramconfig();
+void vendor_load_properties() {
+    set_ramconfig();
     init_alarm_boot_properties();
     variant_properties();
 }
