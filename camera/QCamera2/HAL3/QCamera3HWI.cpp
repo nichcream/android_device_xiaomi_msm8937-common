10c192abfa0cf773c13444df292373e8a8d0cb81
Nich <nctrenco@gmail.com>
2018-07-21 23:17:43 +0800

    land: camera: Don't attempt to trigger buggy backend color correction
     * Known to cause colours that flicker and go off from time to time.
    
    Change-Id: Id8662565ac2228de106f547bc153ae5de4f90239

--- camera/QCamera2/HAL3/QCamera3HWI.cpp
+++ camera/QCamera2/HAL3/QCamera3HWI.cpp
@@ -9407,19 +9407,23 @@ int32_t QCamera3HardwareInterface::extractSceneMode(
         if (NAME_NOT_FOUND != val) {
             uint8_t sceneMode = (uint8_t)val;
             LOGD("sceneMode: %d", sceneMode);
+#if 0
             if (ADD_SET_PARAM_ENTRY_TO_BATCH(hal_metadata,
                     CAM_INTF_PARM_BESTSHOT_MODE, sceneMode)) {
                 rc = BAD_VALUE;
             }
+#endif
         }
     } else if ((ANDROID_CONTROL_MODE_OFF == metaMode) ||
             (ANDROID_CONTROL_MODE_AUTO == metaMode)) {
         uint8_t sceneMode = CAM_SCENE_MODE_OFF;
         LOGD("sceneMode: %d", sceneMode);
+#if 0
         if (ADD_SET_PARAM_ENTRY_TO_BATCH(hal_metadata,
                 CAM_INTF_PARM_BESTSHOT_MODE, sceneMode)) {
             rc = BAD_VALUE;
         }
+#endif
     }
     return rc;
 }
