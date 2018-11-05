8a9c5776a4ce4e8c635a13c6eb817247e1413cd1
Nich <nctrenco@gmail.com>
2018-11-05 14:59:39 +0800

    land: Move QDCM calibration configurations to vendor
    SDM now reads them from /vendor/etc.
    
    Change-Id: I4080070e891d203d261dd6a3b94655ee2cbd2435

--- device.mk
+++ device.mk
@@ -170,6 +170,11 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-service \
     android.hardware.renderscript@1.0-impl
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/qdcm_calib_data_hx8394F_HD720p_video_BOE.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_hx8394F_HD720p_video_BOE.xml \
+    $(LOCAL_PATH)/configs/qdcm_calib_data_ili9881c_HD720p_video_Tianma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_ili9881c_HD720p_video_Tianma.xml \
+    $(LOCAL_PATH)/configs/qdcm_calib_data_nt35521s_HD720p_video_EBBG.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_nt35521s_HD720p_video_EBBG.xml
+
 # Display calibration
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@1.0-service-sdm
