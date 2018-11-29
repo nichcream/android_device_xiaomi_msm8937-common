b94147a67ac3ea2d78940b725ef4e3f6b8e2ec9e
Nich <nctrenco@gmail.com>
2018-11-29 20:57:16 +0800

    land: Update configs for Pie
    Change-Id: I05d81f861a5f51bde3c92f307903ab188fe44351

--- device.mk
+++ device.mk
@@ -67,6 +67,7 @@ PRODUCT_COPY_FILES += \
 # whitelisted app
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml \
+    $(LOCAL_PATH)/configs/privapp-permissions-com.qualcomm.location.xml:system/etc/permissions/privapp-permissions-com.qualcomm.location.xml \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml
 
 # Additional native libraries
