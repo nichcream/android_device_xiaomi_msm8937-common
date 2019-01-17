f57981ca9690b8efd4a7649cd0413f3e0707d41a
Nich <nctrenco@gmail.com>
2019-01-17 19:49:04 +0800

    msm8937-common: Switch back to AOSP charger daemon
    Change-Id: I30744f9b2b72751a856ed9ae8296bc730a420b44

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -100,14 +100,3 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
 include $(BUILD_PREBUILT)
 
 # Configuration scripts
-
-# Offmode charging
-include $(CLEAR_VARS)
-LOCAL_MODULE          := chargeonlymode
-LOCAL_MODULE_OWNER    := xiaomi
-LOCAL_MODULE_PATH     := $(TARGET_ROOT_OUT_SBIN)
-LOCAL_UNSTRIPPED_PATH := $(TARGET_ROOT_OUT_SBIN_UNSTRIPPED)
-LOCAL_SRC_FILES       := sbin/chargeonlymode
-LOCAL_MODULE_TAGS     := optional
-LOCAL_MODULE_CLASS    := EXECUTABLES
-include $(BUILD_PREBUILT)
