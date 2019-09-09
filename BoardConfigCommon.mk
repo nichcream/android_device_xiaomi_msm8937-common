2ee8718675e4396591b91ec34df001ebf59421c8
dianlujitao <dianlujitao@lineageos.org>
2019-09-09 15:04:49 +0800

    msm8937-common: Remove custom kernel cross compile prefix
    Switch back to the 'aarch64-linux-androidkernel-' prefix which is default on
    LineageOS.
    
    Change-Id: Icc58f38fa2f66b833c73902d6733f84b77dcb575

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -29,7 +29,6 @@ BOARD_KERNEL_IMAGE_NAME 	:= Image.gz-dtb
 BOARD_KERNEL_PAGESIZE 		:=  2048
 BOARD_MKBOOTIMG_ARGS 		:= --ramdisk_offset 0x01000000 --second_offset 0x00f00000 --tags_offset 0x00000100
 TARGET_KERNEL_SOURCE 		:= kernel/xiaomi/msm8937
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 TARGET_EXFAT_DRIVER		:= sdfat
 
 # Architecture
